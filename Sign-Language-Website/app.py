e44da2abedb5c8510647e50d67fedff95acaeb75
Varsha222211905 <151942650+varsharavi798@users.noreply.github.com>
2025-02-22 23:33:27 +0530

    Update app.py

--- Sign-Language-Website/app.py
+++ Sign-Language-Website/app.py
@@ -28,4 +28,4 @@ def video_feed():
     return Response(generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
